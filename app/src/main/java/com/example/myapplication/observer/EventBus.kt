2b178ac1acf56eb2f79d7ccd7e68f5495f8d8aff
吴溪 <wux@vecentek.com>
2022-12-29 15:48:36 +0800

    一个简单eventBus

---
+++ app/src/main/java/com/example/myapplication/observer/EventBus.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication.observer
+
+/**
+ *
+ * EventBus
+ * @author Wu Xi
+ * @date 2022/12/29 15:46
+ *
+ */
+class EventBus {
+    private val subscribers = mutableMapOf<Class<*>, MutableList<Subscriber>>()
+
+    fun <T : Any> register(eventType: Class<T>, subscriber: Subscriber) {
+        val subscribersForType = subscribers.getOrPut(eventType) { mutableListOf() }
+        subscribersForType.add(subscriber)
+    }
+
+    fun <T : Any> unregister(eventType: Class<T>, subscriber: Subscriber) {
+        val subscribersForType = subscribers[eventType] ?: return
+        subscribersForType.remove(subscriber)
+    }
+
+    fun post(event: Any) {
+        val eventType = event::class.java
+        val subscribersForType = subscribers[eventType] ?: return
+        for (subscriber in subscribersForType) {
+            subscriber.onEvent(event)
+        }
+    }
+}
